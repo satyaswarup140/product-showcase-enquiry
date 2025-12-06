c465d534d7b09172e91a2cba981119beab7785ac
Your Name <your.email@example.com>
2025-12-06 22:53:49 +0530

    Fixed backend for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,14 @@
-const sqlite3 = require("sqlite3").verbose();
 require("dotenv").config();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database(process.env.DB_FILE, (err) => {
-  if (err) console.error(err);
-  else console.log("SQLite DB connected.");
+const DB_FILE = process.env.DB_FILE || "./database.sqlite";
+
+const db = new sqlite3.Database(DB_FILE, (err) => {
+  if (err) {
+    console.error("Database connection error:", err.message);
+  } else {
+    console.log("SQLite DB connected.");
+  }
 });
 
 module.exports = db;
